3c63f4e2760441e2b2135ec0c3a530be961c3b79
Marinus <marinus.gerdes@gmail.com>
2023-11-23 11:15:45 +0100

    fixed traceroute script

--- traceroute.sh
+++ traceroute.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
-# Überprüfe, ob mindestens zwei Argumente übergeben wurden (Startadresse und mindestens eine Zieladresse)
-if [ $# -lt 2 ]; then
-    echo "Bitte gib mindestens zwei Argumente ein (Startadresse und mindestens eine Zieladresse)."
+# Check if the correct number of arguments is provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <ip-to-traceroute> <hop-ip1> [<hop-ip2> ...]"
     exit 1
 fi
 
-# Startadresse und Zieladressen aus den Argumenten extrahieren
-start_address="$1"
-target_addresses=("${@: -1}")
+# Extract the IP address to traceroute
+target_ip="$1"
+shift
 
-# Führe den Traceroute-Test durch
-traceroute_result=$(traceroute "$start_address")
+# Iterate through the specified hops
+for hop_ip in "$@"; do
+    # Run traceroute and extract the first IP address from the output
+    trace_output=$(traceroute -n -m 1 "$target_ip" | awk '{print $2}' | tail -n 1)
 
-# Array mit erwarteten IP-Adressen initialisieren
-expected_addresses=("${target_addresses[@]}")
-
-# Überprüfe die Hops
-current_hop=1
-for expected_address in "${expected_addresses[@]}"; do
-    # Finde die nächste Hop-Adresse in der Traceroute-Ausgabe
-    #hop_address=$(echo "$traceroute_result" | awk -v hop_number=$current_hop '$0 ~ hop_number {if(NR>1)print $2}')
-    hop_address=$(echo "$traceroute_result" | awk -v hop_number=$current_hop '$0 ~ hop_number {for(i=2;i<=NF;i++){if($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/){print $i; exit}}}')
-
-    # Überprüfe, ob die aktuelle Hop-Adresse mit der erwarteten Adresse übereinstimmt
-    if [ -z "$hop_address" ] || [ "$hop_address" != "$expected_address" ]; then
-        echo "Fehler: Hop $current_hop hat die erwartete Adresse $expected_address nicht."
-	echo "address: $hop_address"
+    # Check if the obtained IP matches the expected hop IP
+    if [ "$trace_output" != "$hop_ip" ]; then
+        echo "Error: Hop $hop_ip not matched. Found: $trace_output"
         exit 1
     fi
-
-    ((current_hop++))
 done
 
-# Alle erwarteten Adressen wurden gefunden
-echo "Alle erwarteten Adressen wurden in den Hops gefunden."
+# If all hops match, print okay
+echo "Okay: All hops matched."
 
